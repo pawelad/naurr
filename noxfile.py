55d1893afe86e94f7ff41e62f2f78a7d0722aeb1
Paweł Adamczak <pawel.ad@gmail.com>
2024-10-16 00:20:54 +0200

    Add base Django and DRF setup

---
+++ noxfile.py
@@ -0,0 +1,56 @@
+"""naurr Nox sessions."""
+
+import nox
+
+nox.options.reuse_existing_virtualenvs = True
+nox.options.error_on_external_run = True
+
+DEFAULT_PATHS = ["src/", "tests/", "noxfile.py"]
+
+
+@nox.session()
+def code_style_checks(session: nox.Session) -> None:
+    """Check code style."""
+    dirs = session.posargs or DEFAULT_PATHS
+
+    # fmt: off
+    session.install(
+        "black", "isort", "ruff", "interrogate",
+        "-c", "requirements/constraints.txt",
+    )
+    # fmt: on
+
+    session.run("black", "--check", "--diff", *dirs)
+    session.run("isort", "--check", "--diff", *dirs)
+    session.run("ruff", "check", "--diff", *dirs)
+    session.run("interrogate", *dirs)
+
+
+@nox.session()
+def type_checks(session: nox.Session) -> None:
+    """Run type checks."""
+    dirs = session.posargs or DEFAULT_PATHS
+
+    # fmt: off
+    session.install(
+        "--no-deps",
+        "-r", "requirements/main.txt",
+        "-r", "requirements/dev.txt",
+    )
+    # fmt: on
+
+    session.run("mypy", *dirs)
+
+
+@nox.session()
+def django_checks(session: nox.Session) -> None:
+    """Run Django checks."""
+    # fmt: off
+    session.install(
+        "--no-deps",
+        "-r", "requirements/main.txt",
+    )
+    # fmt: on
+
+    session.run("src/manage.py", "check", external=True)
+    session.run("src/manage.py", "makemigrations", "--check", external=True)
---
+++ src/naurr/__init__.py
@@ -0,0 +1,9 @@
+"""naurr - Father of Nótt, the personified night."""
+
+__title__ = "naurr"
+__description__ = "Father of Nótt, the personified night."
+__version__ = "0.0.1"
+__url__ = "https://github.com/pawelad/naurr"
+__author__ = "Paweł Adamczak"
+__email__ = "pawel.ad@gmail.com"
+__license__ = "MPL-2.0"
