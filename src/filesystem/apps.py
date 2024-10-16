d0ebf1c3f9d72b703a4e138598b783b9788f470e
Paweł Adamczak <pawel.ad@gmail.com>
2024-10-16 11:56:58 +0200

    Add DRF based API CRUD endpoints

---
+++ src/filesystem/apps.py
@@ -0,0 +1,10 @@
+"""filesystem app config."""
+
+from django.apps import AppConfig
+
+
+class FilesystemConfig(AppConfig):
+    """Django `AppConfig` integration for `filesystem` app."""
+
+    name = "filesystem"
+    verbose_name = "Filesystem"
--- src/naurr/urls.py
+++ src/naurr/urls.py
@@ -5,9 +5,29 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework.authtoken import views as authtoken_views
+
 urlpatterns = [
     # Django Admin
     path("admin/", admin.site.urls),
-    # DRF Auth
-    path("api/auth/", include("rest_framework.urls")),
+    # API
+    path(
+        "api/",
+        include(
+            [
+                # DRF views
+                path(
+                    "auth/",
+                    include("rest_framework.urls", namespace="rest_framework"),
+                ),
+                path(
+                    "auth/obtain_token",
+                    authtoken_views.obtain_auth_token,
+                    name="obtain_token",
+                ),
+                # API
+                path("", include("filesystem.urls", namespace="filesystem")),
+            ]
+        ),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
