cdad8846437ee6f1181189d514c7a60d72b70495
Paweł Adamczak <pawel.ad@gmail.com>
2024-10-16 18:02:43 +0200

    Make `mypy` happy

--- src/naurr/urls.py
+++ src/naurr/urls.py
@@ -3,12 +3,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import URLPattern, URLResolver, include, path
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 from rest_framework.authtoken.views import obtain_auth_token
 
-api_patterns = (
+api_patterns: tuple[list[URLPattern | URLResolver], str] = (
     [
         # API
         path("", include("filesystem.urls", namespace="filesystem")),
